c0619c3fee24509ae8e45ef52aefaabda3e1b7d1
Joseph Prospero <jprospero413@gmail.com>
2021-05-03 04:08:08 -0700

    filled guts of resume section

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -3,18 +3,54 @@ import React from 'react';
 function Resume() {
     return (
         <section className="my-5">
-          <div className="my-2">
-            <h3>Proficiencies:</h3>
-            <li>
-                React
+            <div className="my-2">
+                <h3>Resume</h3>
+                <a href='https://docs.google.com/document/d/13ZvTs1RS_uSv0nL-WZKc5cwrE_x8cuvnOMXqAewNJlw/edit?usp=sharing' target='blank'>
+                    Joseph Prospero
+                </a>
+                <h3>Proficiencies:</h3>
+                <li>
+                    HTML & CSS
             </li>
-            <li>
-                
+                <li>
+                    JavaScript
             </li>
-          </div>
+                <li>
+                    CSS
+            </li>
+                <li>
+                    jQuery
+                </li>
+                <li>
+                    React
+                </li>
+                <li>
+                    Bootstrap
+                </li>
+                <br/>
+                <h4>But wait...THERE'S MORE!</h4>
+                <li>
+                    APIs
+                </li>
+                <li>
+                    Node
+                </li>
+                <li>
+                    Express
+                </li>
+                <li>
+                    MySQL
+                </li>
+                <li>
+                    Sequelize
+                </li>
+                <li>
+                    MongoDB
+                </li>
+            </div>
         </section>
-        
-      );
+
+    );
 }
 
 export default Resume;
