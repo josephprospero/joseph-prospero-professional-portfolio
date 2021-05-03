0f49e2067e7c997c6aab3238fff9e349ec454204
Joseph Prospero <jprospero413@gmail.com>
2021-05-03 03:51:38 -0700

    set up resume component

---
+++ src/components/Resume/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Resume() {
+    return (
+        <section className="my-5">
+          <div className="my-2">
+            <h3>Proficiencies:</h3>
+            <li>
+                React
+            </li>
+            <li>
+                
+            </li>
+          </div>
+        </section>
+        
+      );
+}
+
+export default Resume;
